3b22388e25d7d014943f96791b53666356b73285
Leandro Souza <103716539+leandros8w@users.noreply.github.com>
2022-09-13 20:32:16 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getStorage} from "firebase/storage";
 import { getFunctions } from 'firebase/functions';
 
 const app = initializeApp({
-  apiKey: "AIzaSyAWxrQ8_vdoCevVk_19qvNoaxzbb-hZ8FA",
-  authDomain: "instagram-clone-v8.firebaseapp.com",
-  projectId: "instagram-clone-v8",
-  storageBucket: "instagram-clone-v8.appspot.com",
-  messagingSenderId: "806214029694",
-  appId: "1:806214029694:web:35b770b5817974c64da32f",
-  measurementId: "G-8MQVBQMT53"
+  //configs
 });
 
  const db = getFirestore(app);
